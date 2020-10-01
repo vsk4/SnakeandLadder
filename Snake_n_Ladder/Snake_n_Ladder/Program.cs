00367b005a92df0f4088ab3742cf552336b1bc3c
vsk4 <saikumarvadla4@gmail.com>
2020-10-01 11:29:06 +0530

    [VSK]:Refractor added UC3-Options

--- Snake_n_Ladder/Snake_n_Ladder/Program.cs
+++ Snake_n_Ladder/Snake_n_Ladder/Program.cs
@@ -5,11 +5,31 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            const int noPlayOption = 0;
+            const int ladderOption = 1;
+            const int snakeOption = 2;
             const int playerOne = 1;
             int positionOne = 0;
             Random random = new Random();
             int dieRoll = random.Next(1, 7);
             Console.WriteLine("Die Rolls: " + dieRoll);
+            int options = random.Next(0, 3);
+            switch (options)
+            {
+                case noPlayOption:
+                    positionOne = positionOne + 0;
+                    break;
+                case ladderOption:
+                    positionOne = positionOne + dieRoll;
+                    break;
+                case snakeOption:
+                    positionOne = positionOne - dieRoll;
+                    if (positionOne < 0)
+                    {
+                        positionOne = 0;
+                    }
+                    break;
+            }
         }
     }
 }
