80c607f89c2de4b0b02544a68d60c4593323f099
vsk4 <saikumarvadla4@gmail.com>
2020-10-01 11:05:19 +0530

    [VSK]:Refractor added UC1- singleplayerpostion0

---
+++ Snake_n_Ladder/Snake_n_Ladder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int playerOne = 1;
+            int positionOne = 0;
+        }
+    }
+}
