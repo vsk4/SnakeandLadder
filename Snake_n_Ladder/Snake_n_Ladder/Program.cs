05260ea5b74a5401b1ce52cf0074e6239e6e9b7e
vsk4 <saikumarvadla4@gmail.com>
2020-10-01 11:20:38 +0530

    [VSK]: added UC2-DiceRoll

--- Snake_n_Ladder/Snake_n_Ladder/Program.cs
+++ Snake_n_Ladder/Snake_n_Ladder/Program.cs
@@ -7,6 +7,9 @@ namespace SnakeAndLadder
         {
             const int playerOne = 1;
             int positionOne = 0;
+            Random random = new Random();
+            int dieRoll = random.Next(1, 7);
+            Console.WriteLine("Die Rolls: " + dieRoll);
         }
     }
 }
