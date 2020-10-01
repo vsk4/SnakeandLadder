515ba99fe54793a32967ca0186b9638616c6caba
vsk4 <saikumarvadla4@gmail.com>
2020-10-01 11:44:17 +0530

    [VSK]:Refractor added UC7-TwoPlayers

--- Snake_n_Ladder/Snake_n_Ladder/Program.cs
+++ Snake_n_Ladder/Snake_n_Ladder/Program.cs
@@ -8,39 +8,49 @@ namespace SnakeAndLadder
             const int noPlayOption = 0;
             const int ladderOption = 1;
             const int snakeOption = 2;
-            const int playerOne = 1;
-            int positionOne = 0;
+            int player = 0;
+            int[] playerPosition = new int[2] { 0, 0 };
             int diceRollCount = 0;
-            while (positionOne < 100)
+            while (playerPosition[player] < 100)
             {
                 Random random = new Random();
                 int dieRoll = random.Next(1, 7);
                 diceRollCount++;
                 int options = random.Next(0, 3);
-                Console.WriteLine("Position: " + positionOne);
                 switch (options)
                 {
                     case noPlayOption:
-                        Console.WriteLine("Position: " + positionOne);
+                        Console.WriteLine("Player " + player + "Position: " + playerPosition[player]);
                         break;
                     case ladderOption:
-                        if (positionOne + dieRoll <= 100)
+                        if (playerPosition[player] + dieRoll <= 100)
                         {
-                            positionOne = positionOne + dieRoll;
+                            playerPosition[player] = playerPosition[player] + dieRoll;
                         }
-                        Console.WriteLine("Position: " + positionOne);
+                        Console.WriteLine("Player " + player + "Position: " + playerPosition[player]);
                         break;
                     case snakeOption:
-                        positionOne = positionOne - dieRoll;
-                        if (positionOne < 0)
+                        playerPosition[player] = playerPosition[player] - dieRoll;
+                        if (playerPosition[player] < 0)
                         {
-                            positionOne = 0;
+                            playerPosition[player] = 0;
                         }
-                        Console.WriteLine("Position: " + positionOne);
+                        Console.WriteLine("Player " + player + " Position: " + playerPosition[player]);
                         break;
                 }
+                if (options == noPlayOption || options == snakeOption)
+                {
+                    if (player == 0)
+                    {
+                        player = 1;
+                    }
+                    else
+                    {
+                        player = 0;
+                    }
+                }
             }
-            Console.WriteLine("Player Wins with Die Roll :" + diceRollCount);
+            Console.WriteLine("Player " + player + " Wins with Die Roll :" + diceRollCount);
         }
     }
 }
